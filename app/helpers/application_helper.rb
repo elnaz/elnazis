b908ce7427c4cc6eb22280106ca4763e6c5b6dc3
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2015-01-04 21:11:54 -0600

    page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def navClass(controller)
     "current" if params[:controller] == controller
   end
+  
+  def title(page_title)
+    content_for (:title) { page_title }
+  end
 end
