6088b3e9c776cecab1e5e2c49889a25f1ebf4445
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2015-01-10 16:00:20 -0600

    refactor contact form reset on success

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.valid?
         # send email
-        format.js { render :js =>"$('#contact-notice p').text('Message sent!').fadeOut(3000); $('#new_message')[0].reset();" }
+        format.js { render :js =>"$('#contact-notice p').text('Message sent!').fadeOut(3000); $('#new_message').trigger('reset');" }
       else
       	format.js { render :js =>"$('#contact-notice p').text('There was an error sending your message.');" }
       end  
