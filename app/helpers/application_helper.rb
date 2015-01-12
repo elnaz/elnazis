cb3dd7a6817f20a8531ec52c93038a420735dd01
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2015-01-11 22:10:42 -0600

    update navClass helper to work for about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def navClass(controller)
-    "current" if params[:controller] == controller
+    "current" if params[:controller] == controller || request.path.start_with?('/' + controller)
   end
   
   def title(page_title)
