760779868b71ba4268864c806b61e68c4feb8845
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2015-01-12 00:43:19 -0600

    prepping projects for switch to storing in db

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -16,5 +16,5 @@
 # end
 
 ActiveSupport::Inflector.inflections do |inflect|
-  inflect.uncountable %w( writing )
+  inflect.uncountable %w( working writing )
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get '/working/arctic-collaboration', to: 'working#arctic'
-  get '/working/schoolcuts', to: 'working#schoolcuts'
-  get '/working/benevolent', to: 'working#benevolent'
-  get '/working/chicago-women', to: 'working#cwta'
-  get '/working/student-government', to: 'working#sga'
-  get '/working/alternative-spring-break', to: 'working#asb'
   get '/about', to: 'static_pages#about'
   
+  resources :working
   resources :writing
   resources :messages
 
