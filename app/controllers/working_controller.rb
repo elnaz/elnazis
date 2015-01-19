9d80b4a0eec799bb00ba4471491fc5f966f27554
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2015-01-18 22:34:38 -0600

    password protect working's actions except index and show

--- app/controllers/working_controller.rb
+++ app/controllers/working_controller.rb
@@ -1,4 +1,7 @@
 class WorkingController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"], except: [:index, :show]
+  
   def index
     @workings = Working.all.order(:sort_order)
   end
